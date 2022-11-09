a848783cb35a880a74df923e83bd5d6a7ad2285f
farrelspa <115524400+farrelspa@users.noreply.github.com>
2022-11-09 07:20:15 +0700

    Update do.sh

--- do.sh
+++ do.sh
@@ -20,9 +20,6 @@ case "$PILIHOS" in
 	*) echo "pilihan salah"; exit;;
 esac
 
-echo "Password yang saya buat sudah masuk wordlist bruteforce, silahkan masukkan password yang lebih aman!"
-read -p "Masukkan password untuk akun Administrator (minimal 12 karakter): " PASSADMIN
-
 IP4=$(curl -4 -s icanhazip.com)
 GW=$(ip route | awk '/default/ { print $3 }')
 
@@ -35,7 +32,7 @@ echo CreateObject^("Shell.Application"^).ShellExecute "%~s0", "%*", "", "runas",
 "%temp%\Admin.vbs"
 del /f /q "%temp%\Admin.vbs"
 exit /b 2)
-net user Administrator $PASSADMIN
+net user Administrator ZETESTORE.123
 
 
 for /f "tokens=3*" %%i in ('netsh interface show interface ^|findstr /I /R "Local.* Ethernet Ins*"') do (set InterfaceName=%%j)
@@ -99,4 +96,6 @@ wget https://nixpoin.com/ChromeSetup.exe
 cp -f /tmp/net.bat net.bat
 cp -f /tmp/dpart.bat dpart.bat
 
-echo 'Turn off your server and Boot from HardDrive'
+echo 'Your server will turning off in 3 second'
+sleep 3
+poweroff
