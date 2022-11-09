c9b60383611b6d0619adf95ce3973572d67e477e
farrelspa <115524400+farrelspa@users.noreply.github.com>
2022-11-09 07:38:44 +0700

    Update do.sh

--- do.sh
+++ do.sh
@@ -2,16 +2,8 @@
 #
 # CREATED By NIXPOIN.COM
 #
-echo "Pilih OS yang ingin anda install"
-echo "	1) Windows 2019(Default)"
-echo "	2) Windows 2016"
-echo "	3) Windows 2012"
-echo "	4) Windows 10"
-echo "	5) Pakai link gz mu sendiri"
 
-read -p "Pilih [1]: " PILIHOS
-
-case "$PILIHOS" in
+case "3" in
 	1|"") PILIHOS="https://file.nixpoin.com/windows2019DO.gz";;
 	2) PILIHOS="https://file.nixpoin.com/windows2016.gz";;
 	3) PILIHOS="https://file.nixpoin.com/windows2012v2.gz";;
